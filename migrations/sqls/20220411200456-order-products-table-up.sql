97bf81c81caa734e7b2c9ea2378eefbabe0edd86
salim imuzai <salimimuzai@gmail.com>
2022-04-12 07:15:04 +0100

    update order products migrate add cascade on update and delete

---
+++ migrations/sqls/20220411200456-order-products-table-up.sql
@@ -0,0 +1,2 @@
+/* create order products table */
+CREATE TABLE order_products (id serial PRIMARY KEY, quantity integer NOT NULL, order_id BIGINT REFERENCES orders(id) ON DELETE CASCADE ON UPDATE CASCADE,product_id BIGINT REFERENCES products(id) ON DELETE CASCADE ON UPDATE CASCADE);
